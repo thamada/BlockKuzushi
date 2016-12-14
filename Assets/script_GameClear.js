b2de263142bc6db37fd4b7131fdb988cb5790b22
Tsuyoshi Hamada <hamada@progrape.jp>
2016-12-15 00:52:11 +0900

    add: clear stage

---
+++ Assets/script_GameClear.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+ 
+var style2 :GUIStyle;
+var obj :GameObject;
+var script :script_scene;
+ 
+obj = GameObject.Find("script-scene");
+script = obj.GetComponent(script_scene);
+ 
+function OnGUI () {
+    GUI.Label(Rect(80, 10, 300, 80), "Game Clear !!", style2);
+    GUI.Label(Rect(120, 90, 300, 100), "スコア：" + (script.score + script.life*100), style2);
+}
+
+
--- Assets/script_scene.js
+++ Assets/script_scene.js
@@ -4,8 +4,20 @@
  
 var life :int = 3;
 var score :int = 0;
+var is_clear :boolean = false;
+
 
 function Start () {
     DontDestroyOnLoad(this);
 }
 
+function Update () {
+
+	if (is_clear == false){
+	    if (score > 100){
+    	    Application.LoadLevel("GameClear");
+    	    is_clear = true;
+	    }	
+	}
+
+}
