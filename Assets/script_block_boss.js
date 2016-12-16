b20427cb5ab8918d5a80a147313ab18451250312
Tsuyoshi Hamada <hamada@progrape.jp>
2016-12-17 01:57:23 +0900

    add: block boss

---
+++ Assets/script_block_boss.js
@@ -0,0 +1,25 @@
+#pragma strict
+
+var hp :int;
+
+var obj :GameObject;
+var script :script_scene;
+
+function Start(){
+  obj = GameObject.Find("script-scene");
+  script = obj.GetComponent(script_scene);
+	hp = 1;
+}
+
+function OnCollisionEnter() {
+    hp--;
+    if (hp == 0){
+        Destroy(gameObject);
+        script.score += 8;
+        script.blockCt -= 1;
+				UnityEngine.SceneManagement.SceneManager.LoadScene("GameClear");
+    }else{
+	    script.score += 1;
+    }
+    Debug.Log("hp: "+hp);
+}
