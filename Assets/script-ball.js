1b24c77f6f62eec9df2de6f45f1e5c6aaec62678
Tsuyoshi Hamada <hamada@progrape.jp>
2016-12-19 01:01:39 +0900

    edit: script-ball.js

--- Assets/script-ball.js
+++ Assets/script-ball.js
@@ -3,13 +3,13 @@ var Speed : float = 15.0;
 
 var pos : Vector3;
 var vel : Vector3;
-        
-function Update () {
 
+function Update () {
 //	vel = GetComponent.<Rigidbody>().velocity;
 //	Debug.Log("pos"+transform.position);
 
-    if (Input.GetButtonUp("Jump") && GetComponent.<Rigidbody>().velocity == Vector3(0, 0, 0)){
+//    if (Input.GetButtonUp("Jump") && GetComponent.<Rigidbody>().velocity == Vector3(0, 0, 0)){
+    if (Input.GetButtonUp("Jump")){
         GetComponent.<Rigidbody>().AddForce((transform.forward + transform.right) * Speed, ForceMode.VelocityChange);
     }
 }
@@ -17,7 +17,7 @@ function Update () {
 
 function OnCollisionEnter () {
     GetComponent.<Rigidbody>().velocity = GetComponent.<Rigidbody>().velocity.normalized * 15;
- 
+
     if (Mathf.Abs(GetComponent.<Rigidbody>().velocity.y) < 5) {
         GetComponent.<Rigidbody>().velocity.y *= 5;
     }
@@ -25,4 +25,3 @@ function OnCollisionEnter () {
         GetComponent.<Rigidbody>().velocity.x *= 5;
     }
 }
-
